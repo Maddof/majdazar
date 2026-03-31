3cd2bdb734147c0eea343cd3e112d026c1de4144
Majd A <majd.azar88@gmail.com>
2026-03-31 20:03:06 +0200

    first step animating signature

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,13 @@
 /// <reference types="vite/client" />
-import {
-  HeadContent,
-  Link,
-  Scripts,
-  createRootRoute,
-} from '@tanstack/react-router'
+import { HeadContent, Link, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import * as React from 'react'
 import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary'
 import { NotFound } from '~/components/NotFound'
 import appCss from '~/styles/app.css?url'
 import { seo } from '~/utils/seo'
+
+import { HeaderLinks } from '~/components/header/HeaderLinks'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -23,8 +20,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       ...seo({
-        title:
-          'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
+        title: 'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
         description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
       }),
     ],
@@ -69,60 +65,11 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <div className="p-2 flex gap-2 text-lg">
-          <Link
-            to="/"
-            activeProps={{
-              className: 'font-bold',
-            }}
-            activeOptions={{ exact: true }}
-          >
-            Home
-          </Link>{' '}
-          <Link
-            to="/posts"
-            activeProps={{
-              className: 'font-bold',
-            }}
-          >
-            Posts
-          </Link>{' '}
-          <Link
-            to="/users"
-            activeProps={{
-              className: 'font-bold',
-            }}
-          >
-            Users
-          </Link>{' '}
-          <Link
-            to="/route-a"
-            activeProps={{
-              className: 'font-bold',
-            }}
-          >
-            Pathless Layout
-          </Link>{' '}
-          <Link
-            to="/deferred"
-            activeProps={{
-              className: 'font-bold',
-            }}
-          >
-            Deferred
-          </Link>{' '}
-          <Link
-            // @ts-expect-error
-            to="/this-route-does-not-exist"
-            activeProps={{
-              className: 'font-bold',
-            }}
-          >
-            This Route Does Not Exist
-          </Link>
-        </div>
-        <hr />
-        {children}
+        {/* Only for testing purposes, remove in production */}
+        {/* <HeaderLinks /> */}
+        {/* <hr /> */}
+        <main>{children}</main>
+
         <TanStackRouterDevtools position="bottom-right" />
         <Scripts />
       </body>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
-import { Badge } from '~/components/ui/badge'
 import { buttonVariants } from '~/components/ui/button'
+import SignatureM from '~/components/hero/Signature'
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -8,25 +8,24 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   return (
-    <div className="p-2">
-      <main className="bg-background text-foreground min-h-screen">
-        <section className="mx-auto flex max-w-6xl flex-col gap-12 px-6 py-16 md:px-8 md:py-24">
-          <div className="max-w-3xl space-y-6">
-            <Badge variant="secondary">Available for freelance & junior roles</Badge>
+    <>
+      <section className="relative flex min-h-[55dvh] w-full flex-col gap-12 bg-[url('/images/hero/majd-azar_hero-1.webp')] bg-cover bg-position-[80%_0%] text-white sm:min-h-dvh">
+        <div className="container flex h-full flex-1 items-center">
+          {/* Overlay absolute */}
+          <div className="from-primary/60 to-primary/0 absolute inset-0 z-10 bg-linear-to-r" />
+          <div className="z-20 flex flex-col items-start gap-3">
+            <img
+              src="/images/assets/Vector_stroke.svg"
+              alt="Majd Azar Full Name"
+              className="h-28 w-auto"
+            />
+            <SignatureM />
+            <h1>Majd Azar</h1>
+            <p className="text-[150%]">Full-stack Dev | Founder @ Smokify</p>
 
-            <div className="space-y-4">
-              <p className="text-muted text-sm">Hi, I’m Majd</p>
+            <p className="max-w-xl text-[120%] leading-7">Stockholm, Sweden.</p>
 
-              <h1 className="">Majd Azar</h1>
-              <h2>Full-stack Dev | Founder @ Smokify</h2>
-
-              <p className="text-muted-foreground max-w-2xl text-base leading-7 md:text-lg">
-                This is my personal portfolio where I share selected projects, my stack, and a bit
-                about how I approach building products.
-              </p>
-            </div>
-
-            <div className="flex flex-wrap gap-3">
+            {/* <div className="flex flex-wrap gap-3">
               <Link to="/projects" className={buttonVariants({ variant: 'default' })}>
                 View Projects
               </Link>
@@ -34,10 +33,22 @@ function Home() {
               <Link to="/design" className={buttonVariants({ variant: 'outline' }) + ' border-2'}>
                 View UI Playground
               </Link>
-            </div>
+            </div> */}
           </div>
-        </section>
-      </main>
-    </div>
+        </div>
+      </section>
+      <section className="flex min-h-screen w-full flex-col gap-12 px-6 py-16 md:px-8 md:py-24">
+        <div className="max-w-3xl space-y-6">
+          <h2>More about me</h2>
+
+          <p className="text-muted-foreground max-w-2xl text-base leading-7 md:text-lg">
+            I’m a full-stack developer with a passion for building products that solve real
+            problems. I have experience working with a variety of technologies, including React,
+            Node.js, and TypeScript. I’m currently focused on building my portfolio and looking for
+            new opportunities to grow and learn.
+          </p>
+        </div>
+      </section>
+    </>
   )
 }
